60c2a2a84c348700f29b5ff282fda22764a9188d
achgut <achim.gutsche@web.de>
2022-12-04 14:28:13 +0100

    Datei Ladetest

--- Ladetest.py
+++ Ladetest.py
@@ -6,4 +6,17 @@ import Wallbox_read_Static
 import Wallbox_read_Actual
 
 Wallbox_read_Static
-Wallbox_read_Actual
+
+try:
+  
+  while True :
+    print("Lese")
+    Wallbox_read_Actual
+    print("zurück Go to sleep")
+ 
+    time.sleep(2)
+
+
+except KeyboardInterrupt:
+  print('Hello user you have pressed ctrl-c button.')
+
